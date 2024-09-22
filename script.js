a1ed28cd2f07fa5b17a60eabd47c8a23f003c563
hoken92 <hkenneth92@gmail.com>
2024-09-21 21:46:29 -0700

    added more conditioning to handle individual assignment average

--- script.js
+++ script.js
@@ -84,39 +84,63 @@ function getLearnerData(course, ag, submissions) {
   // 1. capture the courseInfo ID in variable
   const courseInfoID = course.id;
 
+  // checking if assignment course id matches with course id.
+  try {
+    if (ag.course_id != courseInfoID) {
+      throw new Error(
+        `The assignment id: ${ag.id} does not match with this course's id: ${courseInfoID}`
+      );
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
   // AssignmentGroup Data
-  // 1. log course id(number)
-  // 1. individual assignment
-  // 2. log id(number), due dates(string),points(number)
+  //Assignments due
   const deliverables = ag.assignments.filter(function (deliverable) {
     if (deliverable.due_at < "2024-09-20") {
       return true;
     }
   });
 
+  // Assignments that are not due
   const deliverableNotDue = ag.assignments.filter(function (deliverable) {
     if (deliverable.due_at > "2024-09-20") {
       return true;
     }
   });
 
-  //Learner dataq
+  //Learner data
   // 1. log id(number), assignment id, submission(date, score)
-
   let learner = 0;
+
+  // tracks each Learner assignment score
+  let totalScoreAgOne = 0;
+  let totalScoreAgTwo = 0;
+
+  //tracks total Learner combined score
   let totalScore = 0;
-  let possibleScore = 0;
+
+  // tracks combined assignment score
+  let totalPossibleScore = 0;
   let assignmentsTurnedIn = 0;
 
   // Iterating through student assignment submissions
   for (i = 0; i < submissions.length; i++) {
     let students = {};
 
-    if (submissions[i].learner_id === learner) {
-    } else {
+    // tracks individual assignment turned in
+    let assignmentScore = 0;
+    // tracks individual assignment
+    let possibleScore = 0;
+    let newAssignmentTurnedIn = false;
+
+    if (submissions[i].learner_id != learner) {
       learner = submissions[i].learner_id;
+      totalScoreAgOne = 0;
+      totalScoreAgTwo = 0;
       totalScore = 0;
-      possibleScore = 0;
+      totalPossibleScore = 0;
       assignmentsTurnedIn = 0;
     }
 
@@ -129,45 +153,50 @@ function getLearnerData(course, ag, submissions) {
           submissions[i].submission.submitted_at,
           submissions[i].submission.score
         );
+        assignmentScore += checkLateAssignment(
+          deliverables[j].due_at,
+          deliverables[j].points_possible,
+          submissions[i].submission.submitted_at,
+          submissions[i].submission.score
+        );
+
         possibleScore += deliverables[j].points_possible;
+        totalPossibleScore += deliverables[j].points_possible;
         assignmentsTurnedIn++;
+        newAssignmentTurnedIn = true;
+
+        if (newAssignmentTurnedIn) {
+          switch (assignmentsTurnedIn) {
+            case 1:
+              totalScoreAgOne += calcGradeAvg(assignmentScore, possibleScore);
+              break;
+
+            case 2:
+              totalScoreAgTwo += calcGradeAvg(assignmentScore, possibleScore);
+              break;
+          }
+        }
       }
     }
 
+    //checks a submission matching with an assignment not due, it'll skip to the next loop
     if (submissions[i].assignment_id === deliverableNotDue[0].id) {
       continue;
     }
 
     if (assignmentsTurnedIn === deliverables.length) {
+      // if assignment count matches to amount of assignments due, it'll calculate the avg and log the id, avg.
       students.id = learner;
-      students.avg = calcGradeAvg(totalScore, possibleScore);
+      students[deliverables[0].id] = totalScoreAgOne.toFixed(2);
+      students[deliverables[1].id] = totalScoreAgTwo.toFixed(2);
+      students.avg = calcGradeAvg(totalScore, totalPossibleScore);
       learnerResults.push(students);
     }
-    console.log(learner, totalScore);
-    console.log(possibleScore);
-    console.log(assignmentsTurnedIn);
+
+    // console.log(learner, totalScore);
+    // console.log(assignmentsTurnedIn);
+    // console.log(students);
   }
-
-  //   console.log(students);
-
-  // Math operations
-  // 1. avg grade for all assignments submitted per student
-  function calcGradeAvg(learnerPoints, agPoints) {
-    let avg = learnerPoints / agPoints;
-    return avg;
-  }
-
-  // 2. date calcution for assignment not due or late, deduct 10% from score
-  function checkLateAssignment(dueDate, agPoints, subDate, score) {
-    let lateGrade;
-    if (subDate > dueDate) {
-      let deductAmount = agPoints * 0.1;
-      return (lateGrade = score - deductAmount);
-    } else {
-      return score;
-    }
-  }
-
   // Error handling
   // 1. possible points being 0
   // 2. assignment group not matching course id
@@ -190,17 +219,25 @@ function getLearnerData(course, ag, submissions) {
   return learnerResults;
 }
 
+// Calling getLearnerData
 const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
 
 console.log(result);
 
-// loops and objects
-// array methods - map/reduce/filter
+// Math operations
+// 1. avg grade for all assignments submitted per student
+function calcGradeAvg(learnerPoints, agPoints) {
+  let avg = learnerPoints / agPoints;
+  return avg;
+}
 
-// let studentDeliverables = LearnerSubmissions.map(function (studentGrades) {
-//     if (students){
-//         students[]
-//     } else {
-//         if ( )
-//     }
-//   });
+// 2. date calcution for assignment not due or late, deduct 10% from score
+function checkLateAssignment(dueDate, agPoints, subDate, score) {
+  let lateGrade;
+  if (subDate > dueDate) {
+    let deductAmount = agPoints * 0.1;
+    return (lateGrade = score - deductAmount);
+  } else {
+    return score;
+  }
+}
